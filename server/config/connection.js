71a4ef303848f200b1e040eb5ff535615e950f12
James Reilly <jreilly2112@gmail.com>
2022-06-01 04:01:21 -0400

    mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cluster0.bbxhnbu.mongodb.net', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
